d218b197070c92febe6e717494de81dc8fcc2a45
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2023-01-09 00:59:33 +0000

    breytingar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hvað er Verðbólgudraugurinn?</h1>
+    
+    <p>
+      Þegar verð á vöru og þjónustu hækkaði um tugi prósenta á ári á síðustu öld var oft talað um að verðbólgudraugurinn væri komin á kreik og skyti fólki skelk í bringu.
+      Nú er öldin önnur. Síðustu ár hefur verðbólga verið nálægt tveimur og hálfu prósenti sem er verðbólgumarkmið sem Seðlabankinn og ríkisstjórn hafa ákveðið að stefna að.
+      En það er með verðbólguna eins og ýmsar veirur og sjúkdóma: Ef við gætum okkar ekki og förum óvarlega í efnahagsmálum getur verðbólgudraugurinn risið upp aftur og valdið usla eins og áður.
+      Það má því segja að þótt verðbólgudraugurinn sé ekki sýnilegur í dag þá sé hann eins og vírus sem liggur í dvala og getur skotið upp kollinum aftur.
+    </p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    /* min-height: 100vh; */
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    /* align-items: center; */
   }
 }
 </style>
